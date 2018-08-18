09d13d797912d8904be3b462327949ab63c51964
Invoker zengshuang <631710518@qq.com>
2018-08-18 15:11:02 +0800

    process.py

--- processWikiPku.py
+++ processWikiPku.py
@@ -1,11 +1,19 @@
 import mysql.connector
-
+from printProcess import show
+import datetime
 
 def process(cursor, sql, output):
     cursor.execute(sql)
+    a = cursor.fetchall()
+    start = datetime.datetime.now()
     output_dict = {}
-    for pageId, subject, property_name, objectType, value in cursor:
-        print("{}\t{}\t{}\t{}\t{}".format(pageId, subject, property_name, objectType,value))
+    total = len(a) - 1
+    c = 0
+    for property_name, objectType, value in a:
+        # print("{}\t{}\t{}".format(property_name, objectType,value))
+        c+=1
+        show(c, total, interval=datetime.datetime.now() - start,
+             DoneInfo='Process finished.')
         property_value_list = []
         if objectType == '_metre':
             value = str(value).split('-')[0]
@@ -23,17 +31,21 @@ def process(cursor, sql, output):
 
 
 if __name__ == '__main__':
+    '''
     wiki_conn = mysql.connector.connect(host='localhost', port='3306', user='root', password='icstwip',
                                         database='JWPL_zh_050602')
     wiki_cursor = wiki_conn.cursor()
-    process(wiki_cursor, ' select pageId, subject, predicate, objectType, value from tripleValue ',
+    process(wiki_cursor, ' select predicate, objectType, value from tripleValue ',
             'results/wiki_result')
     wiki_conn.close()
     wiki_cursor.close()
+    '''
 
-    pku_conn = mysql.connector.connect(host='localhost', port='3306', user='root', password='icstwip',
+    pku_conn = mysql.connector.connect(host='59.108.48.35', port='3306', user='root', password='icstwip',
                                        database='knowledgeGift')
     pku_cursor = pku_conn.cursor()
-    process(pku_cursor, ' select pageId, subject, predicate, objectType, value from tripleValue ', 'results/pku_result')
+    # pku_cursor.execute(' select predicate, objectType, value from tripleValue ', 'results/pku_result ')
+    # a = pku_cursor.fetchall()
+    process(pku_cursor, ' select predicate, objectType, value from tripleValue ', 'results/pku_result')
     pku_conn.close()
     pku_cursor.close()
